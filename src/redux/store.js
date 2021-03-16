7353d8a36685b3efa5e3e6aa292ef05de666a307
mirjSolution <ricojohndatoon23@gmail.com>
2021-03-16 12:14:54 -0400

    Logger Middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import rootReducer from './rootReducer';
-// import cakeReducer from './cakes/cakeReducer';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
